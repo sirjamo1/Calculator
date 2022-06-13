0ca539a21a3914a710b9eb80cf67b13f60bfe2be
Sirjamo1 <jam_how@hotmail.com>
2022-06-13 11:06:42 +1000

    * Added keyboard support * cleaned up html/css/js *added to readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Calculator
+
+<h3>update : 13/06/2022</h3>
+<br><br>
+<h4>Features</h4>
+<ul>
+<li>Keyboard support</li>
+<li>Buttons have animation and sound when clicked</li>
+<li>Maximum of 12 digits can be entered or displayed on screen</li>
+<li>"Too Long!" will be displayed if answer is too long for screen/Infinite</li>
+<li>Only one decimal point can be added before clicking an operator (again before clicking equals)</li>
+<li>Answer will only be displayed to four decimal places</li>
+<li>Clear will clear everything</li>
+<li>Can use answer with another operator</li>
+</ul>
+
+<br><br>
+
+<h4>Take A-ways</h4>
+<ul>
+<li>A better understanding of position</li>
+<li>Usage of onclick="storeInput(this)" in html</li>
+<li>Work around to adding keyboard support through onclick()</li>
+<li>A better understanding of :active in css</li>
+<li>Usage of regex</li>
+<li>Importance of changing from integer to string and reverse</li>
+<li></li>
+</ul>
+
+<br><br>
+
+<h4>Sounds from www.freesound.org</h4>
+<ul>
+<li>Button sound by: ueffects</li>
+</ul>
--- js/script.js
+++ js/script.js
@@ -45,7 +45,6 @@ function maths(operator, tempValue) {
       return divide(tempValue);
   }
 }
-
 const keySound = new Audio("sounds/ueffects__a-key.wav");
 let displayValue = [];
 let operator = [];
@@ -54,8 +53,9 @@ let answer = [];
 document.getElementById("screenInput").innerHTML = "0";
 
 function storeInput(e) {
+  let buttonInput = [];
   keySound.play();
-  let buttonInput = e.getAttribute("value");
+  buttonInput = e.getAttribute("value");
   //IF BUTTON IS NUMBER ADD / IF BUTTON IS "." ONLY 0NE "." CAN BE ADDED
   if (
     (/^\d*\.?\d+$/.test(buttonInput) || /^\.?$/.test(buttonInput)) &&
@@ -64,7 +64,7 @@ function storeInput(e) {
     if (answer.length > 0) {
       displayValue = [];
     }
-    if (displayValue.indexOf(".") >= 1 && /^\.?$/.test(buttonInput)) {
+    if (displayValue.indexOf(".") > -1 && /^\.?$/.test(buttonInput)) {
       document.getElementById("screenInput").innerHTML = `${displayValue}`;
     } else {
       document.getElementById("screenInput").innerHTML = `${(displayValue +=
@@ -97,7 +97,7 @@ function storeInput(e) {
       tempValue.push(displayValue);
       tempValue = tempValue.map(Number);
       answer = maths(operator, tempValue);
-      answer = Math.round(answer * 100) / 100; //<<decimal place condition
+      answer = Math.round(answer * 10000) / 10000; //<<decimal place condition (4)
       answer = answer.toString();
       operator = [];
       tempValue = [];
@@ -116,10 +116,81 @@ function storeInput(e) {
       }
     }
   }
-  // console.log("-------------------------------");
-  // console.log({ buttonInput });
-  // console.log({ operator });
-  // console.log({ displayValue });
-  // console.log({ tempValue });
-  // console.log({ answer });
+}
+
+//LISTENS FOR KEYBOARD GRABS THE ID THEN RUNS ONCLICK TO RUN STOREINPUT FUNCTION
+window.addEventListener("keydown", keyboard);
+function keyboard(e) {
+  let x = e.key;
+  let selection = [];
+  switch (x) {
+    case "0":
+      selection = document.querySelector("#zero");
+      selection.click();
+      break;
+    case "1":
+      selection = document.querySelector("#one");
+      selection.click();
+      break;
+    case "2":
+      selection = document.querySelector("#two");
+      selection.click();
+      break;
+    case "3":
+      selection = document.querySelector("#three");
+      selection.click();
+      break;
+    case "4":
+      selection = document.querySelector("#four");
+      selection.click();
+      break;
+    case "5":
+      selection = document.querySelector("#five");
+      selection.click();
+      break;
+    case "6":
+      selection = document.querySelector("#six");
+      selection.click();
+      break;
+    case "7":
+      selection = document.querySelector("#seven");
+      selection.click();
+      break;
+    case "8":
+      selection = document.querySelector("#eight");
+      selection.click();
+      break;
+    case "9":
+      selection = document.querySelector("#nine");
+      selection.click();
+      break;
+    case "Escape":
+      selection = document.querySelector("#clear");
+      selection.click();
+      break;
+    case "/":
+      selection = document.querySelector("#divide");
+      selection.click();
+      break;
+    case "*":
+      selection = document.querySelector("#multiply");
+      selection.click();
+      break;
+    case "-":
+      selection = document.querySelector("#minus");
+      selection.click();
+      break;
+    case "+":
+      selection = document.querySelector("#plus");
+      selection.click();
+      break;
+    case "Enter":
+      selection = document.querySelector("#equal");
+      selection.click();
+      break;
+    case ".":
+      selection = document.querySelector("#decimal");
+      selection.click();
+      
+  }
 }
